f06a18656c303338a324da48a314ae10cff71473
CHEEZ-BURGER <s73042728@gmail.com>
2021-12-04 04:10:26 +0900

    header 40%

---
+++ mainPage.js
@@ -0,0 +1,12 @@
+function btn_1() {
+  let up_1 = document.querySelector(".up_bar");
+  let down_1 = document.querySelector(".down_bar");
+
+  up_1.classList.toggle("active");
+  down_1.classList.toggle("active");
+}
+function btn_2() {
+  let scerchIcon = document.querySelector(".scerch_btn");
+
+  scerchIcon.classList.toggle("active");
+}
