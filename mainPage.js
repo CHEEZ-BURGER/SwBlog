36d76960f58a8264027fb2ec9a669192a4cede34
CHEEZ-BURGER <s73042728@gmail.com>
2021-12-05 00:10:24 +0900

    header_90%

--- mainPage.js
+++ mainPage.js
@@ -1,9 +1,15 @@
 function btn_1() {
   let up_1 = document.querySelector(".up_bar");
   let down_1 = document.querySelector(".down_bar");
+  let header = document.querySelector(".header");
+  let scerchIcon = document.querySelector(".scerch_btn");
+  let header_hidden = document.querySelector(".header_hidden");
 
   up_1.classList.toggle("active");
   down_1.classList.toggle("active");
+  header.classList.toggle("active");
+  header_hidden.classList.toggle("active");
+  scerchIcon.classList.toggle("up");
 }
 function btn_2() {
   let scerchIcon = document.querySelector(".scerch_btn");
