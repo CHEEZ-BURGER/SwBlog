405f4449965c5b68a87bf54f83d369aedc7d8294
CHEEZ-BURGER <s73042728@gmail.com>
2022-02-14 05:55:11 +0900

    blogLog Add_2

---
+++ BlogLog.js
@@ -0,0 +1,14 @@
+function dark(){
+var sections = document.querySelectorAll(".nfixed_back, .cat_btn_box a, .back_btn a, .content_box_blur, .main_inner, .hello_p, .bold, .high, .more_box_p, .main_inner h1, .arrow_right, body, h1, h2, .recent_text h3, .logo, .scerch_btn, .main_inner h2, .header_logo, .main_inner h2, .main_inner p, .main_inner li, p, .main_inner ul, .main_cont_back, .footer, .back_btn img, .line, .hello_content_box p, .header"); 
+for( var i = 0; i < sections.length; i++ ){
+    var item = sections.item(i); 
+    item.classList.remove("dark");
+    }
+}
+function light(){
+    var sections = document.querySelectorAll(".nfixed_back, .cat_btn_box a, .back_btn a, .content_box_blur, .main_inner, .hello_p, .bold, .high, .more_box_p, .main_inner h1, .arrow_right, body, h1, h2, .recent_text h3, .logo, .scerch_btn, .main_inner h2, .header_logo, .main_inner h2, .main_inner p, .main_inner li, p, .main_inner ul, .main_cont_back, .footer, .back_btn img, .line, .hello_content_box p, .header"); 
+    for( var i = 0; i < sections.length; i++ ){
+        var item = sections.item(i); 
+        item.classList.add("dark");
+        }
+    }
